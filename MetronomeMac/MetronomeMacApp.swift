6ba8a564071e1fe3953239856c4a68d3f5bc7095
chrisfinns <cf4856@gmail.com>
2024-11-28 09:08:15 +0900

    Initial Commit

---
+++ MetronomeMac/MetronomeMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeMacApp.swift
+//  MetronomeMac
+//
+//  Created by Chris  Finnell on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
